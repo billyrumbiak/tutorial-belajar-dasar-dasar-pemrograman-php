5d54dfd598bd2b8e4aba1aa3710617ac9903f4e6
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2023-06-17 15:41:34 +0900

    tambah echo-print.php

---
+++ echo-print.php
@@ -0,0 +1,11 @@
+<?php
+
+    // echo
+    echo "<h1>Papuan Tech Comunity</h1>";
+    echo "<p>Belajar dasar pemrograman.</p>";
+
+    // print
+    print "<h1>Papuan Coding School</h1>";
+    print "<p>Belajar coding bersama komunitas.</p>"
+
+?>
