5480ef7801e16d2a518c2be424c03f7d1e73f9e7
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2023-06-17 15:34:09 +0900

    tambah variables.php

---
+++ variables.php
@@ -0,0 +1,19 @@
+<?php
+    // Contoh penulisan variables di dalam PHP
+
+    $title = "SaCode";
+    $tag_lane = "Papuan Coding Schools";
+    $years = "2023";
+
+    // Menampilkan data dari dalam variable
+
+    echo $title;
+    echo "<br>";
+
+    echo $tag_lane;
+    echo "<br>";
+
+    echo $years;
+    echo "<br>";
+
+?>
