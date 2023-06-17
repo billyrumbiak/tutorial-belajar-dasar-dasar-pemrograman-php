e3b7247d382e58ed41c0e97d69675a18beb422f1
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2023-06-17 15:04:00 +0900

    Tambah html-php.php

---
+++ html-php.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kombinasi HTML dan PHP</title>
+</head>
+<body>
+
+    <h1>Kombinasi HTML dan PHP</h1>
+
+    <?php
+        echo "Hello PHP!";
+    ?>
+    
+</body>
+</html>
